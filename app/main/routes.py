71804816e6876811b31e20320995a44858b934f2
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-08-16 09:26:44 -0500

    feat: new pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,4 +5,19 @@ from app.main import bp
 
 @bp.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html", title="Home", pagina_actual="HOME")
+    return render_template("home.html", title="Home")
+
+
+@bp.route("/white", methods=["GET"])
+def white():
+    return render_template("white.html")
+
+
+@bp.route("/black", methods=["GET"])
+def black():
+    return render_template("black.html")
+
+
+@bp.route("/green", methods=["GET"])
+def green():
+    return render_template("green.html")
