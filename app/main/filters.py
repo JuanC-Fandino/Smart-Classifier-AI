351bd238b6a6d9ba31cfd5a8ebb6cb597a71ba89
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-10-25 12:01:31 -0500

    fix: utc time

--- app/main/filters.py
+++ app/main/filters.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from app.main import bp
 
 
 @bp.app_template_filter('utc_to_local')
-def utc_to_local(utc_dt: datetime) -> datetime:
-    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None).time().strftime('%H:%M')
+def utc_to_local(utc_dt: datetime) -> str:
+    tz = timezone(timedelta(hours=-5))
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=tz).time().strftime('%H:%M')
