0b2afebdb7625e379678159cd8a544b36c0a0753
Juan Camilo Fandiño Orjuela <juancamilo1998@hotmail.com>
2022-11-22 07:36:08 -0500

    feat: new dashboard

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 import base64
+import datetime
 
-from flask import render_template, request, Response
+from flask import render_template, request, Response, jsonify
 from flask_login import login_required, current_user
 
 from app import db
@@ -92,34 +93,80 @@ def dashboard():
     predictions = PredictionRecord.query.count()
     average_confidence = PredictionRecord.query.with_entities(db.func.avg(PredictionRecord.confidence)).scalar()
     all_confidence = PredictionRecord.query.with_entities(PredictionRecord.confidence).all()
-    # calculate the standard deviation
     average_confidence = round(average_confidence, 2)
     all_confidence = [confidence[0] for confidence in all_confidence]
     standard_deviation = round((sum([(confidence - average_confidence) ** 2 for confidence in all_confidence]) / (len(
         all_confidence) - 1)) ** 0.5, 2)
 
+    predictions_with_feedback = PredictionRecord.query.filter(PredictionRecord.isAccurate != None).count()
+    correct_predictions = PredictionRecord.query.filter_by(isAccurate=True).count()
+    correct_predictions_percentage = round(correct_predictions / predictions_with_feedback * 100, 2)
+
     return render_template("dashboard.html", title="Dashboard", users=users, predictions=predictions,
-                           average_confidence=average_confidence, standard_deviation=standard_deviation)
+                           average_confidence=average_confidence, standard_deviation=standard_deviation,
+                           correct_predictions_percentage=correct_predictions_percentage)
 
 
 @bp.route("/dashboard/statistics_per_day", methods=["GET"])
 @login_required
 def statistics_per_day():
-    # group the predictions by day
     records = PredictionRecord.query.with_entities(
         db.func.date(PredictionRecord.datetime),
         db.func.count(PredictionRecord.datetime)).group_by(
         db.func.date(PredictionRecord.datetime)).all()
 
-    print(records)
     results_array = []
     for record in records:
         results = {"datetime": record[0], "count": record[1]}
         results_array.append(results)
 
-    print(results_array)
+    return jsonify(results_array)
 
-    return results_array
+
+@bp.route("/dashboard/users_per_day", methods=["GET"])
+@login_required
+def users_per_day():
+    records = User.query.with_entities(
+        db.func.date(User.created_at),
+        db.func.count(User.created_at)).group_by(
+        db.func.date(User.created_at)).where(
+        User.created_at >= f'{datetime.datetime.utcnow() - datetime.timedelta(days=7)}').all()
+
+    results_array = []
+    for record in records:
+        results = {"datetime": record[0], "count": record[1]}
+        results_array.append(results)
+
+    return jsonify(results_array)
+
+
+@bp.route("/dashboard/predictions_per_type", methods=["GET"])
+@login_required
+def predictions_per_type():
+    records = PredictionRecord.query.with_entities(
+        PredictionRecord.prediction_type,
+        db.func.count(PredictionRecord.prediction_type)).group_by(
+        PredictionRecord.prediction_type).all()
+
+    results_array = []
+    for record in records:
+        results = {"type": record[0].name, "count": record[1]}
+        results_array.append(results)
+
+    return jsonify(results_array)
+
+
+@bp.route("/dashboard/percentage_accurate_predictions/<prediction_type>", methods=["GET"])
+@login_required
+def percentage_accurate_predictions(prediction_type):
+    predicitons_with_feedback = PredictionRecord.query.filter(PredictionRecord.isAccurate != None, PredictionRecord.prediction_type == prediction_type).count()
+    correct_predictions = PredictionRecord.query.filter_by(isAccurate=True, prediction_type=prediction_type).count()
+    try:
+        correct_predictions_percentage = round(correct_predictions / predicitons_with_feedback * 100, 2)
+    except ZeroDivisionError:
+        correct_predictions_percentage = "No hay datos suficientes"
+
+    return jsonify(correct_predictions_percentage)
 
 
 @bp.route("/accuracy", methods=["POST"])
--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,10 +1,9 @@
-let classes = [];
-let yArray = [];
-
 fetch('/dashboard/statistics_per_day', {
     method: 'GET',
 }).then(response => response.json())
     .then(data => {
+        let classes = [];
+        let yArray = [];
         for (let i = 0; i < data.length; i++) {
             let date = new Date(data[i].datetime);
             classes.push(date.toISOString().split("T")[0]);
@@ -13,15 +12,106 @@ fetch('/dashboard/statistics_per_day', {
         let allData = [{
             x: classes,
             y: yArray,
-            type: "scatter"
+            type: "scatter",
+            line: {
+                color: "#28a745",
+            }
         }];
 
         const layout = {
             title: 'Número de predicciones por día',
+            font: {
+                size: 16
+            }
         };
         const config = {responsive: true}
 
         Plotly.newPlot("myPlot", allData, layout, config);
 
     });
+
+fetch('/dashboard/users_per_day', {
+    method: 'GET',
+}).then(response => response.json())
+    .then(data => {
+        let classes = [];
+        let yArray = [];
+        for (let i = 0; i < data.length; i++) {
+            let date = new Date(data[i].datetime);
+            classes.push('[' + date.toISOString().split("T")[0] + ']');
+            yArray.push(data[i].count);
+        }
+        let allData = [{
+            x: classes,
+            y: yArray,
+            type: "bar",
+            width: [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+            marker: {
+                color: "#28a745",
+            }
+        }];
+
+        const layout = {
+            title: 'Usuarios registrados por día (última semana)',
+            font: {
+                size: 16
+            }
+        };
+        const config = {responsive: true}
+
+        Plotly.newPlot("usersPlot", allData, layout, config);
+
+    });
+
+fetch('/dashboard/predictions_per_type', {
+    method: 'GET',
+}).then(response => response.json())
+    .then(data => {
+        let classes = [];
+        let yArray = [];
+        for (let i = 0; i < data.length; i++) {
+            classes.push(data[i].type);
+            yArray.push(data[i].count);
+        }
+        let allData = [{
+            labels: classes,
+            values: yArray,
+            type: "pie",
+            automargin: true,
+        }];
+
+        const layout = {
+            title: 'Predicciones por tipo',
+            font: {
+                size: 16
+            }
+        };
+        const config = {responsive: true}
+
+        Plotly.newPlot("predictionsPlot", allData, layout, config);
+        const pieChart = document.getElementById('predictionsPlot');
+
+        pieChart.on('plotly_click', function (data) {
+            let predictionType = data.points[0].label;
+            fetch('/dashboard/percentage_accurate_predictions/' + predictionType, {
+                method: 'GET',
+            }).then(response => response.json())
+                .then(data => {
+                    let container = document.getElementById('custom_type');
+                    container.style.display = 'block';
+                    let kpi_label = document.getElementById('type_label');
+                    kpi_label.innerHTML = 'Predicciones Correctas (' + predictionType + ')';
+                    let kpi_value = document.getElementById('type_value');
+                    let value = parseFloat(data);
+                    if (isNaN(value)) {
+                        kpi_value.innerHTML = 'No hay datos';
+                    } else {
+                        kpi_value.innerHTML = value + '%';
+                    }
+
+            });
+        });
+
+    });
+
 
